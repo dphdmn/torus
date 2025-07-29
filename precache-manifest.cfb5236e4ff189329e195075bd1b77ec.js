4571e2945e8846defa18c28c357d7c328f01223e
dphdmn <pwfan1000@gmail.com>
2025-07-30 03:07:06 +0800

    Initial commit of all project files

---
+++ precache-manifest.cfb5236e4ff189329e195075bd1b77ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c90519096ec1f3f4df9",
+    "url": "css/app.87448dd6.css"
+  },
+  {
+    "revision": "d5ba4e75966e5bc367f8",
+    "url": "css/chunk-00a0dc9e.6007a33b.css"
+  },
+  {
+    "revision": "40e7dd356766f97af81b",
+    "url": "css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "d4fd8c24d349900e8f03624bafe72e80",
+    "url": "index.html"
+  },
+  {
+    "revision": "9c90519096ec1f3f4df9",
+    "url": "js/app.ed85fc45.js"
+  },
+  {
+    "revision": "d5ba4e75966e5bc367f8",
+    "url": "js/chunk-00a0dc9e.6c246bdc.js"
+  },
+  {
+    "revision": "40e7dd356766f97af81b",
+    "url": "js/chunk-vendors.aad5a76f.js"
+  },
+  {
+    "revision": "c0cf9ac2f1614c1ce36f7b1b204e4a89",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "7079f5a17d8bbd1be51e425d90424899",
+    "url": "oauth-callback.html"
+  }
+]);
